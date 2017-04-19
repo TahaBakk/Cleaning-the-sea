b767a9f834b9319885c5780af1e2dc03827abc97
TahaBakk <tahabakk2@gmail.com>
2017-04-19 13:52:12 +0200

    Pausa
    Añadido script para poder pausar el juego

---
+++ Assets/Scripts/pause.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class pause : MonoBehaviour {
+
+	private bool paused = false;
+	public Text text;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("p") && paused == false)
+		{
+			paused = true;
+			Time.timeScale = 0;
+			text.text = "Game paused";
+		}else if(Input.GetKeyDown("p") && paused == true)
+		{
+			paused = false;
+			Time.timeScale = 1;
+			text.text = "";
+
+		}
+	}
+}
