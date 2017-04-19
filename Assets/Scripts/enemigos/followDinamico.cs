e8703beca33991b9b1a4fe240c6a223f6cdfd2f0
TahaBakk <tahabakk2@gmail.com>
2017-04-19 13:17:00 +0200

    enemigos
    creado script para que el enemigo se mueva de un target a otro para que
    se pueda mover por el laberinto

---
+++ Assets/Scripts/enemigos/followDinamico.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class followDinamico : MonoBehaviour {
+
+	public Transform[] target;//Le pasamos los target a los que queremos moverlo
+	public float speed;
+
+	private int current;
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position != target[current].position)
+		{
+			Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+			GetComponent<Rigidbody>().MovePosition(pos);
+		}else current = (current+1) % target.Length;
+	}
+
+	void OnTriggerEnter(Collider other) 
+     {
+         if (other.tag == "target")
+         {
+			 current = (current+1) % target.Length;
+         }
+     }
+}
