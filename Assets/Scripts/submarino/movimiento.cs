e61646004cff2b426a00926fc4a41cc951435e04
TahaBakk <tahabakk2@gmail.com>
2017-04-19 11:50:53 +0200

    Nave y pickUp
    echos los efectos para la nave sobre el agua y pickUP para conseguir los
    puntos

---
+++ Assets/Scripts/submarino/movimiento.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour {
+
+	public float speed; //= 1000f;
+	public float velRotacion; //= 1000f;
+	public float aceleracion;// = 1000f;
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		rigidbody.AddTorque(0f,horizontal*velRotacion*Time.deltaTime,0f);
+		rigidbody.AddForce(transform.forward*vertical*aceleracion*Time.deltaTime*-1);
+		/*float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+		Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+		GetComponent<Rigidbody>().AddForce(direction * speed);//añadir la  velocidad dependiendo hacia donde le demos*/
+	}
+
+	void OnTriggerEnter(Collider other) 
+     {
+         if (other.tag == "pickup")
+         {
+             Destroy(other.gameObject);
+			//count++;
+
+			// updateCounter();
+         }
+     }
+
+}
---
+++ Assets/Scripts/submarino/pickup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour {
+
+	private int count;
+//	public Text text;
+
+	void Start (){
+		count = 0;
+		//updateCounter();
+		//winText.text = "";
+	}
+
+	void OnTriggerEnter(Collider other) 
+     {
+         if (other.tag == "pickup")
+         {
+             Destroy(other.gameObject);
+		//count++;
+
+			// updateCounter();
+         }
+     }
+	//Para gestionar los puntos
+	/* void updateCounter(){
+		text.text = "Puntos: " + count;
+		int numPickups = GameObject.FindGameObjectsWithTag("pickup").Length;
+
+		if(numPickups == 1){
+			winText.text = "¡Ganaste!";
+	 }
+	 }*/
+
+}
